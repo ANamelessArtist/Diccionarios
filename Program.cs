65a67e317c0dc6c7732bf210607a6058052177ee
Lab2-14 <Lab2-14@LAB2-14>
2025-03-13 18:29:47 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diccionarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Cartas = new Dictionary<int, string>();
+            Cartas.Add(1, "Espadas");
+            Cartas.Add(3, "Espadas");
+            Cartas.Add(4, "Espadas");
+            Cartas.Add(7, "Diamantes");
+            Cartas.Add(8, "Diamantes");
+            Cartas.Add(9, "Diamantes");
+            Cartas.Add(10, "Corazones");
+            Cartas.Add(11, "Corazones");
+            Cartas.Add(12, "Clubs");
+            Cartas.Add(2, "Clubs");
+        }
+    }
+}
